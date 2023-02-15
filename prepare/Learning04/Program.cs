96355ac9b9253eb3ff344d21d02a4a1bf390b1f9
20mthorpe <marydancer1120@gmail.com>
2023-02-15 12:47:42 -0700

    prepare week 7

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        System.Console.WriteLine();
+        Assignment assign1 = new Assignment("Mary Thorpe", "Muliplication tables");
+        Console.WriteLine(assign1.GetSummary());
+
+        MathAssignment math1 = new MathAssignment("John Doe", "Long Division", "5.2", "12-30");
+        System.Console.WriteLine(math1.GetSummary());
+        System.Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment write1 = new WritingAssignment("Grace Pettit", "Family Relations", "Conflict Resolution");
+        System.Console.WriteLine(write1.GetSummary());
+        System.Console.WriteLine(write1.GetWritingInformation());
+
+        System.Console.WriteLine();
     }
 }
