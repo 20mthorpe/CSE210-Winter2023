b5eaefed5c1e086000da8fb6cef4fa356bce38d1
20mthorpe <marydancer1120@gmail.com>
2023-03-14 22:43:43 -0600

    progress on this assingment... yikes

--- prove/Develop05/ChecklistGoal.cs
+++ prove/Develop05/ChecklistGoal.cs
@@ -1,8 +1,20 @@
 class ChecklistGoal : Goal
 {
 
-    public ChecklistGoal(int points, string name, string desc, bool isComplete) : base(points, name, desc, isComplete)
+    public int _numToCompletion = 0;
+    public int _bonusPoints = 0;
+    public int _timesCompleted = 0;
+    public ChecklistGoal(int points, string name, string desc, bool isComplete, int numToCompletion, int bonusPoints) : base(points, name, desc, isComplete)
     {
-
+        _numToCompletion = numToCompletion;
+        _bonusPoints = bonusPoints;
+    }
+    public override void DisplayGoal()
+    {
+        System.Console.WriteLine($"{_name} ({_desc}) -- Currently Completed {_timesCompleted}/{_numToCompletion}");
+    }
+    public void CompleteEventOnce()
+    {
+        _timesCompleted++;
     }
 }
