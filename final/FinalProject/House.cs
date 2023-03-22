b11bb03057ca6ee957216d34575b221cd5cca05f
20mthorpe <marydancer1120@gmail.com>
2023-03-22 16:46:13 -0600

    student/pet setup

--- final/FinalProject/House.cs
+++ final/FinalProject/House.cs
@@ -5,14 +5,18 @@ class House
     private List<string> _colors;
     private string _mascot;
     private string _commonRoomEntrance;
+    private string _ghost;
+    private string _founder;
 
-    public House(string name, string mascot, string commonRoomEntrance, List<string> values, List<string> colors)
+    public House(string name, string mascot, string commonRoomEntrance, List<string> values, List<string> colors, string ghost, string founder)
     {
         _name = name;
         _mascot = mascot;
         _commonRoomEntrance = commonRoomEntrance;
         _values = values;
         _colors = colors;
+        _ghost = ghost;
+        _founder = founder;
     }
 
 }
