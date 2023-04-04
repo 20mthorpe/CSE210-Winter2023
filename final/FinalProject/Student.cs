4e0525d5d3102772b870d2d29166323e5289ff67
20mthorpe <marydancer1120@gmail.com>
2023-04-04 11:52:51 -0600

    Add a menu to the end

--- final/FinalProject/Student.cs
+++ final/FinalProject/Student.cs
@@ -12,12 +12,54 @@ class Student
         _year = year;
         _house = house;
     }
+    public House GetHouse()
+    {
+        return _house;
+    }
     public void SetPet(Pet pet)
     {
         _pet = pet;
     }
+    public Pet GetPet()
+    {
+        return _pet;
+    }
     public void SetWand(Wand wand)
     {
         _wand = wand;
+    }
+    public void DisplayStudentInfo()
+    {
+        System.Console.WriteLine($"Your name is {_name}");
+
+        if(_year == 1)
+        {
+            System.Console.WriteLine($"You are in your first year.");
+        }
+        else if(_year == 2)
+        {
+            System.Console.WriteLine($"You are in your second year.");
+        }
+        else if(_year == 3)
+        {
+            System.Console.WriteLine($"You are in your third year.");
+        }
+        else if(_year == 4)
+        {
+            System.Console.WriteLine($"You are in your fourth year.");
+        }
+        else if(_year == 5)
+        {
+            System.Console.WriteLine($"You are in your fifth year.");
+        }
+        else if(_year == 6)
+        {
+            System.Console.WriteLine($"You are in your sixth year.");
+        }
+        else if(_year == 7)
+        {
+            System.Console.WriteLine($"You are in your seventh year.");
+        }
+        
     }
 }
